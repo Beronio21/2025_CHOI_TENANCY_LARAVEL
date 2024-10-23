297c8f04eb2bd5710aac169cbd8aa3ed20a4f860
Beronio21 <2201107938@student.buksu.edu.ph>
2024-10-23 22:19:23 +0800

    add node js

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Import routes
+const studentRoutes = require('./routes/student');
+const thesisRoutes = require('./routes/thesis');
+const notificationRoutes = require('./routes/notification');
+const messageRoutes = require('./routes/message');
+
+// Use routes
+app.use('/api', studentRoutes);
+app.use('/api', thesisRoutes);
+app.use('/api', notificationRoutes);
+app.use('/api', messageRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
